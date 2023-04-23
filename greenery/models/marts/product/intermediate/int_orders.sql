99e37c17ee5f81c3bb78c73aefd962b07165a36e
Martin Cepeda <mcepeda@instawork.com>
2023-04-23 21:23:49 +0000

    int test

--- greenery/models/marts/product/intermediate/int_orders.sql
+++ greenery/models/marts/product/intermediate/int_orders.sql
@@ -7,6 +7,9 @@
 SELECT 
     order_id
     , user_id
+    , u.first_name
+    , u.last_name
+    , u.address
     , promo_id
     , address_id
     , created_at
@@ -19,3 +22,5 @@ SELECT
     , delivered_at
     , status
 FROM {{ ref( 'stg_orders') }}
+
+LEFT JOIN {{ ref('int_users')}} u ON u.user_id = o.user_id
