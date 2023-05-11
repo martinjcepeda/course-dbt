0bf4f3e3bf32a07d49956e47543c7dd782070b9c
Martin Cepeda <mcepeda@instawork.com>
2023-05-11 05:48:23 +0000

    update week 3

---
+++ greenery/Week 3 README.md
@@ -0,0 +1,9 @@
+## Week 3 Project 
+
+##### Conversion Rate 
+Total Sessions = 578 
+Checkout Sessions = 361 
+Conversion Rate = 0.624
+
+##### Conversion Rate by Product 
+
--- greenery/models/marts/product/int_orders.sql
+++ greenery/models/marts/product/int_orders.sql
@@ -6,11 +6,11 @@
 select
   o.order_id,
   oi.product_id,
+  p.name,
   oi.quantity,
   o.user_id,
   u.first_name, 
   u.last_name,
-  u.address,
   promo_id,
   o.address_id,
   o.created_at,
@@ -24,3 +24,4 @@ select
 from {{ ref( 'stg_orders') }} o
   left join {{ ref( 'stg_order_items') }} oi on oi.order_id = o.order_id
   left join {{ ref('stg_users')}} u ON u.user_id = o.user_id
+  left join {{ ref('stg_products')}} p ON p.product_id = oi.product_id
